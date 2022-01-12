f258aed63530d0b26e753b14622c4b115d7c0d3c
Grant Chen <33037186+ChenGrant@users.noreply.github.com>
2022-01-12 11:04:09 -0500

    Update models.py

--- webpage/models.py
+++ webpage/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     description = models.CharField(max_length = 100, default = "")
-    price = models.DecimalField(max_digits = 9, decimal_places = 2)
-    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits = 9, decimal_places = 2, validators=[MinValueValidator(0)])
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
 
     def __str__(self):
         return self.description
